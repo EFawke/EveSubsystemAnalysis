1488989cebfbeb7431a07f8055e31ad9c7eddbe1
Eduardo Fawke <teduardof@gmail.com>
2024-09-08 17:55:03 +0100

    s

--- backend/api/apiRouter.js
+++ backend/api/apiRouter.js
@@ -3,16 +3,16 @@ const apiRouter = express.Router();
 
 require('./materialsCalculator.js');
 
-// const homeRouter = require('./homeRouter.js');
-// require('./matCostUpdater.js');
-// require('./marketDataUpdater.js');
-// require('./zkillUpdater.js');
-// require('./homePageUpdater.js');
-// const subsystemRouter = require('./subsystemRouter.js');
-// const marketRouter = require('./marketRouter.js');
+const homeRouter = require('./homeRouter.js');
+require('./matCostUpdater.js');
+require('./marketDataUpdater.js');
+require('./zkillUpdater.js');
+require('./homePageUpdater.js');
+const subsystemRouter = require('./subsystemRouter.js');
+const marketRouter = require('./marketRouter.js');
 
-// apiRouter.use('/subsystem', subsystemRouter);
-// apiRouter.use('/home', homeRouter);
-// apiRouter.use('/market', marketRouter);
+apiRouter.use('/subsystem', subsystemRouter);
+apiRouter.use('/home', homeRouter);
+apiRouter.use('/market', marketRouter);
 
 module.exports = apiRouter;
