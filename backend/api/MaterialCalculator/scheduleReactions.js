fde276c6486022a27a4b829397aceb729019b141
Eduardo Fawke <teduardof@gmail.com>
2025-04-19 20:57:57 +0100

    schedule algo

--- backend/api/MaterialCalculator/scheduleReactions.js
+++ backend/api/MaterialCalculator/scheduleReactions.js
@@ -1,20 +1,18 @@
-// this code is fucking sick
-
-const runScheduleAlgorithm = (percentageStep, sortedReactions, meanRuns, slots, minimumAllowedEmptySlots, mode) => {
+const runScheduleAlgorithm = (numAboveMean, sortedReactions, meanRuns, slots) => {
     let schedule = [];
     let availableSlots = slots;
-    const meanRunsPlusXPercent = Math.round(((meanRuns / 100) * percentageStep) + meanRuns);
+    const meanRunsPlusX = meanRuns + numAboveMean;
     for (let i = 0; i < sortedReactions.length; i++) {
-        const numberOfInitialRuns = Math.floor(sortedReactions[i].runs / meanRunsPlusXPercent);
-        const numberLeftOver = sortedReactions[i].runs - (meanRunsPlusXPercent * numberOfInitialRuns);
+        const numberOfInitialRuns = Math.floor(sortedReactions[i].runs / meanRunsPlusX);
+        const numberLeftOver = sortedReactions[i].runs - (meanRunsPlusX * numberOfInitialRuns);
         for (let j = 0; j < numberOfInitialRuns; j++) {
             schedule.push({
                 name: sortedReactions[i].name,
-                runs: meanRunsPlusXPercent,
+                runs: meanRunsPlusX,
             });
             availableSlots -= 1;
         }
-        if (sortedReactions[i].runs % meanRunsPlusXPercent !== 0) {
+        if (sortedReactions[i].runs % meanRunsPlusX !== 0) {
             schedule.push({
                 name: sortedReactions[i].name,
                 runs: numberLeftOver,
@@ -22,49 +20,26 @@ const runScheduleAlgorithm = (percentageStep, sortedReactions, meanRuns, slots, 
             availableSlots -= 1;
         }
     }
-
-    if (mode === "strict") {
-        if (availableSlots != minimumAllowedEmptySlots) {
-            if (percentageStep < 200) {
-                return runScheduleAlgorithm(percentageStep + 1, sortedReactions, meanRuns, slots, minimumAllowedEmptySlots, "strict");
-            } else {
-                return runScheduleAlgorithm(0, sortedReactions, meanRuns, slots, 1, "liberal");
-            }
-        } else {
-            return schedule;
-        }
+    if (availableSlots >= 0) {
+        return schedule;
+    } else {
+        return runScheduleAlgorithm(numAboveMean + 1, sortedReactions, meanRuns, slots);
     }
-
-    if (mode === "liberal") {
-        if (availableSlots >= 0 && availableSlots >= minimumAllowedEmptySlots) {
-            return schedule;
-        } else {
-            if (percentageStep < 200) {
-                return runScheduleAlgorithm(percentageStep + 1, sortedReactions, meanRuns, slots, 1, "liberal");
-            }
-        }
-    }
-};
-
-const scheduleReactions = (reactions, slots) => {
+ };
+ const scheduleReactions = (reactions, slots) => {
     let schedule = false;
-
-    if(slots > 120){
-        slots = 120; // restricting a bullshit number of slots here
+    if(slots > 1000){
+        slots = 1000; // Users probably won't need more than 1000 slots but stops trolling
     }
     const sortedReactions = reactions.sort((a, b) => a.runs - b.runs)
     const reacRunsSum = sortedReactions.reduce((acc, curr) => acc + Number(curr.runs), 0);
     const meanRuns = Math.round(reacRunsSum / slots)
-    let percentageChange = 0;
-
-    if(slots <= sortedReactions.length){
+    const numAboveMean = 0;
+    if (slots <= sortedReactions.length) {
         return sortedReactions;
     } else {
-        const minimumAllowedEmptySlots = 0;
-        schedule = runScheduleAlgorithm(percentageChange, sortedReactions, meanRuns, slots, minimumAllowedEmptySlots, "strict")
+        schedule = runScheduleAlgorithm(numAboveMean + 1, sortedReactions, meanRuns, slots);
     }
-
     return schedule;
-}
-
-module.exports = {scheduleReactions};
+ }
+ module.exports = {scheduleReactions};
